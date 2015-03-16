7633f702e7777c58232a894495600a36d41c9e1a
anoworl <anoworl@gmail.com>
2015-03-16 18:29:07 +0900

    compatible aws sdk v2

--- lib/elbow/capistrano.rb
+++ lib/elbow/capistrano.rb
@@ -14,12 +14,12 @@ def elastic_load_balancer(name, *args)
   include Capistrano::DSL
 
   aws_region= fetch(:aws_region, 'us-east-1')
-  AWS.config(:access_key_id => fetch(:aws_access_key_id),
+  Aws.config(:access_key_id => fetch(:aws_access_key_id),
              :secret_access_key => fetch(:aws_secret_access_key),
              :ec2_endpoint => "ec2.#{aws_region}.amazonaws.com",
              :elb_endpoint => "elasticloadbalancing.#{aws_region}.amazonaws.com")
 
-  load_balancer = AWS::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == elb_name(name) }
+  load_balancer = Aws::ELB.new.load_balancers.find { |elb| elb.dns_name.downcase == elb_name(name) }
   raise "EC2 Load Balancer not found for #{name} in region #{aws_region}" if load_balancer.nil?
 
   load_balancer.instances.each do |instance|
